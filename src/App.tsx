63b6707326c33e9286361c80a540f2c8ad333dba
jfernandesdev <jfernandes.dev@gmail.com>
2021-09-14 21:53:50 -0300

    creation and styling of the home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.scss'
+import './styles/landing.scss';
+
+import logoHappy from './images/logo.svg';
+import homeIllustration from './images/ilustracao-happy.svg';
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoHappy} alt="Logo Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Lavras</strong>
+          <span>Minas Gerais</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+      </div>
+    </div>
   );
 }
 
